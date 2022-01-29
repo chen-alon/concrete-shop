8ae4a663fa94f7d8064178785c83be1bff02de9d
chen-alon <chenalon259@gmail.com>
2022-01-29 11:40:23 +0200

    Make GA to work on production only

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -6,16 +6,21 @@ export const initGA = () => {
 };
 
 export const logPageView = () => {
-  // console.log(`Logging pageview for ${window.location.pathname}`);
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
+  if (process.env.NODE_ENV === "production" && process.browser) {
+    console.log(`Logging pageview for ${window.location.pathname}`);
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  }
 };
 
 export const logEvent = (category = '', action = '') => {
-  // console.log("GA event:", category, ":", action);
-  if (category && action) {
-    ReactGA.event({ category, action });
+  if (process.env.NODE_ENV === "production" && process.browser) {
+    console.log("GA event:", category, ":", action);
+    if (category && action) {
+      ReactGA.event({ category, action });
+    }
   }
+
 };
 
 export const logException = (description = '', fatal = false) => {
